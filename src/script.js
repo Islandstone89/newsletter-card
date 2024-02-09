03cc63e55abfa4469c8b6db9aff6ce041a95fbdc
Øystein Håberg <75746434+Islandstone89@users.noreply.github.com>
2024-02-09 21:35:39 +0100

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -4,14 +4,34 @@
 
 // When clicked, .success should appear
 
+let email = document.querySelector(".email");
+let button = document.querySelector(".btn");
+let successMessage = document.querySelector(".success");
+
+let form = document.querySelector(".form");
 
 
-const button = document.querySelector(".btn");
+email.addEventListener("input", () => {
+    if(email.validity.valueMissing) {
+        email.setCustomValidity("This field cannot be empty");
+    }
+    else if(email.validity.typeMismatch) {
+        email.setCustomValidity("Please enter a valid email address");
+    }
+    else {
+        email.setCustomValidity("");
+    }
+});
 
 function showMessage() {
-    const successMessage = document.querySelector(".success");
-    successMessage.textContent = "Success!";
-    successMessage.style.display = "block";
+    if(email.checkValidity()) {
+        alert("Success!");
+    }
 }
 
 button.addEventListener("click", showMessage);
+
+
+
+
+
